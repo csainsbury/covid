7e0546ea648a71c777347f940008c33dce4589b8
rjmulholland <66015072+rjmulholland@users.noreply.github.com>
2020-08-28 23:37:14 +0100

    Update Stacking_RF_XGB_NB_LR.py

--- Stacking_RF_XGB_NB_LR.py
+++ Stacking_RF_XGB_NB_LR.py
@@ -57,7 +57,6 @@ tf.random.set_seed(2)
 sc = StandardScaler()
 
 #
-n_samples = 10000
 kfold_splits = 5
 n_epochs = 40
 
@@ -77,8 +76,6 @@ kf = KFold(n_splits= kfold_splits)
 kf.get_n_splits(unique_IDs)
 print(kf)
 
-cvscores, aucs, losses, val_losses, acc_ = [], [], [], [], []
-#
 for index, (train_indices, val_indices) in enumerate(kf.split(unique_IDs)):
     print("Training on fold " + str(index+1) + "/" + str(kfold_splits) + "...")
     print("TRAIN:", train_indices, "TEST:", val_indices)
@@ -113,136 +110,13 @@ for index, (train_indices, val_indices) in enumerate(kf.split(unique_IDs)):
     scalers = StandardScaler()
     X_train = scalers.fit_transform(X_train)
     X_val = scalers.transform(X_val)
-#
-## FCL setup
-    dense_N = 36
-    dropout_n = 0.8
-    n_batch_size = 1024 # 64
-#
-    # a = input the hour 0 dataset (2-dimensional: IDs, timesteps)
-    time_point_data = Input(shape = (len(X_train[1]), ), dtype='float32', name = 'time_point_data')
-#
-    fcl_process = Dense(dense_N)(time_point_data)
-    fcl_process = Dense(dense_N)(fcl_process)
-    fcl_process = Dropout(dropout_n)(fcl_process)
-    fcl_process = Dense(dense_N)(fcl_process)
-    fcl_process = Dropout(dropout_n)(fcl_process)
-#
-    main_output = Dense(1, activation = 'sigmoid')(fcl_process)
-
-#fit the model on the COVID dataset
-def fit_model(X_train, y_train):
-#define model
-    model = Model(inputs=[time_point_data], outputs=main_output)
-#
-    print(model.summary())
-#
-    adam = optimizers.Adam(lr=0.0001, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.0)
-#
-    model.compile(optimizer = adam, loss='binary_crossentropy', metrics=['accuracy'])
-#
-    class_weight = {0: 1.,
-                    1: cw, # 1: 20.
-                    }
-    histories = my_callbacks.Histories()
-    #model fit
-    model.fit([X_train], y_train, epochs=n_epochs, batch_size=n_batch_size, validation_data = ([[X_val], y_val]),  class_weight=class_weight, callbacks = [histories])
-    model.save('base_nn.h5')
-    return model
-
-
-
-
-
-#fit and save the models
-n_members = 5
-for i in range(n_members):
-    #fit model
-    model = fit_model(X_train, y_train)
-    # save models
-    filename = '/Users/ryanjmulholland/documents/nnmodels/model_' + str(i + 1) + '.h5'
-    model.save(filename)
-
-# load models from file
-def load_all_models(n_models):
-    all_models = list()
-    for i in range(n_models):
-        # define filename for this ensemble
-        filename = '/Users/ryanjmulholland/documents/nnmodels/model_' + str(i + 1) + '.h5'
-        # load model from file
-        model = load_model(filename)
-        # add to list of members
-        all_models.append(model)
-        print('>loaded %s' % filename)
-    return all_models
-
-# define stacked model from multiple member input models
-def define_stacked_model(members):
-    # update all layers in all models to not be trainable
-    for i in range(len(members)):
-        model = members[i]
-        for layer in model.layers:
-            # make not trainable
-            layer.trainable = False
-            # rename to avoid 'unique layer name' issue
-            layer.name = 'ensemble_' + str(i + 1) + '_' + layer.name
-    # define multi-headed input
-    ensemble_visible = [model.input for model in members]
-    # concatenate merge output from each model
-    ensemble_outputs = [model.output for model in members]
-    merge = concatenate(ensemble_outputs)
-    hidden = Dense(10, activation='sigmoid')(merge)
-    output = Dense(1, activation='sigmoid')(hidden)
-    model = Model(inputs=ensemble_visible, outputs=output)
-    # plot graph of ensemble
-    plot_model(model, show_shapes=True, to_file='model_graph.png')
-    # compile
-    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-    return model
-
-# fit a stacked model
-def fit_stacked_model(model, inputX, inputy):
-    # prepare input data
-    X = [inputX for _ in range(len(model.input))]
-    # fit model
-    model.fit(X, inputy, epochs=300, verbose=0)
-
-# make a prediction with a stacked model
-def predict_stacked_model(model, inputX):
-    # prepare input data
-    X = [inputX for _ in range(len(model.input))]
-    # make prediction
-    return model.predict(X, verbose=0)
-
-# load all models
-n_members = 5
-members = load_all_models(n_members)
-print('Loaded %d models' % len(members))
-
-# define ensemble model
-stacked_model = define_stacked_model(members)
-
-
-# fit stacked model on test dataset
-fit_stacked_model(stacked_model, X_val, y_val)
-# make predictions and evaluate
-yhat = predict_stacked_model(stacked_model, X_val)
-yhat = argmax(yhat, axis=1)
-acc = accuracy_score(y_val, yhat)
-print('Stacked Test Accuracy: %.3f' % acc)
-
-model.save('stacked_model.h5')
-
-
-def get_model():
-    return load_model('stacked_model.h5')
 
 
 # Logistic regression
 clf_lr = LogisticRegression(C =1.5, solver='sag', fit_intercept=True)
 
-# SVM Model
-clf_svm = SVC(C=0.01, gamma=0.1, kernel="poly", degree=3, coef0=10.0, probability=True)
+# Naive Bayes Model
+clf_nb = GaussianNB()
 
 # XGBoost Model
 clf_XGB = XGBClassifier(n_estimators=500,
